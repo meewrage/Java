035b892a2a09bc9da9b4b83d7f8d0994d3c9a952
meewrage <luki_wop@mail.ru>
2023-06-28 00:11:33 +0300

    added 1 task

---
+++ seminars/HW/task1.java
@@ -0,0 +1,21 @@
+package seminars.HW;
+
+import java.util.Scanner;
+
+//1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+//2) Вывести все простые числа от 1 до 1000
+//3) Реализовать простой калькулятор
+public class task1 {
+    public static void main(String[] args) {
+        Scanner inputRead = new Scanner(System.in); 
+        int n = inputRead.nextInt();
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        
+        System.out.println(result);
+        }
+
+inputRead.close();    
+}
+}
