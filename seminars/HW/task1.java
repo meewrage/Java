13cbd624dd41f7ee932d0d397107df1281d55efb
meewrage <luki_wop@mail.ru>
2023-06-28 15:43:09 +0300

    added 2 task

--- seminars/HW/task1.java
+++ seminars/HW/task1.java
@@ -3,7 +3,7 @@ package seminars.HW;
 import java.util.Scanner;
 
 //1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
-//3) Реализовать простой калькулятор
+
 public class task1 {
     public static void main(String[] args) {
         Scanner inputRead = new Scanner(System.in); 
---
+++ seminars/HW/task3.java
@@ -0,0 +1,5 @@
+package seminars.HW;
+
+public class task3 {
+    //3) Реализовать простой калькулятор
+}
